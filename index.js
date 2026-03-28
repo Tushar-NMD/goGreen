f69b27c51656bcf87e40b57719bab6b463a32289
Tushar-NMD <tussharrpr@gmail.com>
2026-03-29 00:29:04 +0530

    green

--- index.js
+++ index.js
@@ -5,36 +5,31 @@ import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
-};
-
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return simpleGit().push();
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
+  // Generate a random timestamp strictly before now
+  const now = moment();
+  const daysAgo = random.int(1, 365); // at least 1 day ago
+  const hoursAgo = random.int(0, 23);
+  const minutesAgo = random.int(0, 59);
+  const secondsAgo = random.int(0, 59);
+
+  const date = now
+    .subtract(daysAgo, "days")
+    .subtract(hoursAgo, "hours")
+    .subtract(minutesAgo, "minutes")
+    .subtract(secondsAgo, "seconds")
+    .format("YYYY-MM-DDTHH:mm:ss");
+
+  const data = { date };
+  console.log("Commit on:", date);
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommits.bind(this, --n));
   });
 };
 
-makeCommits(100);
+makeCommits(50); // adjust number of commits
